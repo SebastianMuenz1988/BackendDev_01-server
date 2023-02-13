1a601e512251b29c9f4824e0e370e98b0662b1a4
SebastianMuenz1988 <sebastian.muenz.24@gmail.com>
2023-02-13 17:52:27 +0100

    Commit-Message: Initial project files

---
+++ src/server.js
@@ -0,0 +1,28 @@
+// If you want to use the more modern "import" syntax, remember to add "type": "module" to package.json
+const express = require("express");
+const { indexRoute, helloRoute } = require("./routes/basic.js");
+const {
+  allProductsRoute,
+  specificProductRoute,
+  deleteProductRoute,
+} = require("./routes/fruits.js");
+// Deconstructing an object: const { x, y } = object
+
+const app = express();
+
+// Router
+// The callback function takes a request and a response object as arguments.
+app.get("/", indexRoute);
+
+app.get("/hello", helloRoute);
+
+app.get("/fruits", allProductsRoute);
+
+app.get("/fruits/:index", specificProductRoute);
+
+app.delete("/fruits/:index", deleteProductRoute);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
